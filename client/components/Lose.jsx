ea877bfbb1b5867000aeed6fc9cf6289626f86d9
Samuel Hoe Yan Looi <samhylooi@gmail.com>
2019-11-08 11:59:08 +1300

    added all routes for results

--- client/components/Lose.jsx
+++ client/components/Lose.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Lose = () => {
-
-
   return (
     <div>
-      
+      <img className="villainsWin" src="/Images/villains_win.jpg" />
+      <h1 className="resultHeading">Villains Win!</h1>
     </div>
+
   )
 }
 
