36f12c9fc00e31d074874950ce96c8c8c0909476
Samuel Hoe Yan Looi <samhylooi@gmail.com>
2019-11-08 10:43:03 +1300

    installed nodemon & router

---
+++ client/components/Draw.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Draw = () => {
+
+
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default Draw
---
+++ client/components/Win.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const Win = () => {
+
+  console.log('THIS IS THE W')
+  return (
+    <h1>asdasd</h1>
+    // <div>asdasd</div>
+    // <div style={{ backgroundImage: url('/images/heros_win.jpg') }}>
+    //   <h1 className="resultHeading">You Win!</h1>
+
+    // </div>
+  )
+}
+
+export default Win
