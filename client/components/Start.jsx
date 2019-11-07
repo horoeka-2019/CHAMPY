3a1910e48652a6811e6e11907e50793494f17534
Jackie Shuyu <jackie.shuyu@gmail.com>
2019-11-08 12:02:59 +1300

    create start button

--- client/components/Start.jsx
+++ client/components/Start.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Start = () => {
   return (
-    <button type="button" className="startButton" value="start"></button>
+    <React.Fragment>
+      <img></img>
+      <Link to="/game">
+        <input type="submit" className="startButton" value="start"></input>
+      </Link>
+    </React.Fragment>
   )
 }
 
