455f2e45fea5c22778a977b1918410d0c0e53e08
Te Piha Toeke Raymond Wii Niha <ttrwwn@hotmail.com>
2019-11-08 12:18:22 +1300

    onclick function enlarges the heros and villains

--- client/components/Hero.jsx
+++ client/components/Hero.jsx
@@ -1,30 +1,59 @@
-import React from 'React'
+import React from 'react'
 
-const Hero = (props) => {
-  return (
-    <React.Fragment>
-      <div>
-        <img src="../../server/public/Images/Heros/Deku.jpeg" height="40" width="Auto" />
-      </div>
+class Hero extends React.Component {
+  state = {
+    height0: 200,
+    height1: 200,
+    height2: 200,
+    height3: 200,
+    height4: 200
 
-      {/* <div>
-        <img />
-      </div>
+  }
 
-      <div>
-        <img />
-      </div>
+  expandDeku = (hero) => {
+    this.setState({ height0: 300 })
+  }
+  expandAllMight = (hero) => {
+    this.setState({ height1: 300 })
+  }
+  expandBakugo = (hero) => {
+    this.setState({ height2: 300 })
+  }
+  expandOchaco = (hero) => {
+    this.setState({ height3: 300 })
+  }
+  expandTodoroki = (hero) => {
+    this.setState({ height4: 300 })
+  }
 
-      <div>
-        <img />
-      </div>
+  render () {
+    return (
+      <React.Fragment>
+        <h2>Hero's up, on a tuesday</h2>
 
-      <div>
-        <img />
-      </div> */}
+        <div onClick={this.expandDeku} >
+          <img src="/Images/Heros/Deku.jpeg" alt="deku" height={this.state.height0} width="Auto"/>
+        </div>
 
-    </React.Fragment>
-  )
+        <div onClick={this.expandAllMight}>
+          <img src="/Images/Heros/AllMight.jpeg" alt="deku" height={this.state.height1} width="Auto" />
+        </div>
+
+        <div onClick={this.expandBakugo}>
+          <img src="/Images/Heros/Bakugo.jpeg" alt="deku" height={this.state.height2} width="Auto" />
+        </div>
+
+        <div onClick={this.expandOchaco}>
+          <img src="/Images/Heros/Ochaco.jpeg" alt="deku" height={this.state.height3} width="Auto" />
+        </div>
+
+        <div onClick={this.expandTodoroki}>
+          <img src="/Images/Heros/Todoroki.jpeg" alt="deku" height={this.state.height4} width="Auto" />
+        </div>
+
+      </React.Fragment>
+    )
+  }
 }
 
 export default Hero
---
+++ client/components/Villian.jsx
@@ -0,0 +1,65 @@
+import React from 'react'
+
+class Villian extends React.Component {
+  state = {
+    height0: 200,
+    height1: 200,
+    height2: 200,
+    height3: 200,
+    height4: 200
+
+  }
+
+  expandShigaraki = (hero) => {
+    this.setState({ height0: 300 })
+  }
+  expandAllforOne = (hero) => {
+    this.setState({ height1: 300 })
+  }
+  expandHimikoToga = (hero) => {
+    this.setState({ height2: 300 })
+  }
+  expandStain = (hero) => {
+    this.setState({ height3: 300 })
+  }
+  expandDabi = (hero) => {
+    this.setState({ height4: 300 })
+  }
+
+  // randomNumber = () => {
+  //   const Randy = Math.randomNumber()
+
+  //   console.log(Randy)
+  // }
+
+  render () {
+    return (
+      <React.Fragment>
+        <h2>Hero's up, on a tuesday</h2>
+
+        <div onClick={this.expandShigaraki}>
+          <img src="/Images/Villians/Shigaraki.png" alt="deku" height={this.state.height0} width="Auto" />
+        </div>
+
+        <div onClick={this.expandAllforOne}>
+          <img src="/Images/Villians/AllforOne.png" alt="deku" height={this.state.height1} width="Auto" />
+        </div>
+
+        <div onClick={this.expandHimikoToga}>
+          <img src="/Images/Villians/HimikoToga.png" alt="deku" height={this.state.height2} width="Auto" />
+        </div>
+
+        <div onClick={this.expandStain}>
+          <img src="/Images/Villians/Stain.png" alt="deku" height={this.state.height3} width="Auto" />
+        </div>
+
+        <div onClick={this.expandDabi}>
+          <img src="/Images/Villians/Dabi.jpeg" alt="deku" height={this.state.height4} width="Auto" />
+        </div>
+
+      </React.Fragment>
+    )
+  }
+}
+
+export default Villian
