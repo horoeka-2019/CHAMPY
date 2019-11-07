893af0b6147bbead74871f233aabd6b430b29988
Adam Cowie <cowie.adamj@gmail.com>
2019-11-08 10:22:41 +1300

    images folder

---
+++ client/components/Hero.jsx
@@ -0,0 +1,30 @@
+import react from 'react'
+
+const Hero = (props) => {
+  return (
+    <React.Fragment>
+      <div>
+        <img />
+      </div>
+      
+      <div>
+        <img />
+      </div>
+
+      <div>
+        <img />
+      </div>
+
+      <div>
+        <img />
+      </div>
+
+      <div>
+        <img />
+      </div>
+b
+    </React.Fragment>
+  )
+}
+
+export default Hero
