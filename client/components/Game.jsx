bf000997a347197b030227322ba50ddf8784d908
Te Piha Toeke Raymond Wii Niha <ttrwwn@hotmail.com>
2019-11-08 13:29:40 +1300

    we are chaning the route of the calculation into hero into villian and then calculate

--- client/components/Game.jsx
+++ client/components/Game.jsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import Hero from './Hero'
-import Villian from './Villian'
 
 const Game = (props) => {
   return (
     <React.Fragment>
       <h1>Im here boi </h1>
       <Hero />
-      <Villian />
 
     </React.Fragment>
   )
